7733782f0770b312b6c65e44e9bbbde84b71caf8
RMTAditya07 <137287608+RMTAditya07@users.noreply.github.com>
2023-10-04 22:02:40 +0530

    Update Mars_Rover.java

--- Mars_Rover.java
+++ Mars_Rover.java
@@ -21,7 +21,6 @@ class Coordinate {
 }
 
 //The class Grid represents the boundaries of the rectangle grid. It contains two piece of information [width and height]. They are private. Hence can be only accessed within the class.
-
 //It has two methods [getWidth, getHeight] to find out the width and height
 class Grid{
     private int width; 
