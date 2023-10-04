8bcb681c97c6a07bf1fcb7cfbcd50aecc585503a
RMTAditya07 <21f1005024@ds.study.iitm.ac.in>
2023-10-04 21:49:09 +0530

    Created Coordinate Class

---
+++ Mars_Rover.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Coordinate {
+    private int x;
+    private int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
