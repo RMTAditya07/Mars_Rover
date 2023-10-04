915bae638ab9448788fa85f76734ec39fb5ec4dd
RMTAditya07 <21f1005024@ds.study.iitm.ac.in>
2023-10-04 22:26:33 +0530

    Added interfaces

--- Mars_Rover.java
+++ Mars_Rover.java
@@ -62,3 +62,20 @@ class BoundaryViolationException extends Exception {
         super(message);
     }
 }
+
+
+//It is a contract that classes can choose to follow.
+//Any class that claims to be Moveable must have a move method.
+//The same applies to all other interfaces.
+interface Movable {
+    void move() throws BoundaryViolationException;
+}
+
+interface Turnable {
+    void turnLeft();
+    void turnRight();
+}
+
+interface Positionable {
+    Coordinate getCurrentPosition();
+}
